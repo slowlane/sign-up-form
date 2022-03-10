d3ae57c4b7c0f1a166052c84af1aae52ecbe340f
PerennialWill <andreas.wickerfalt@gmail.com>
2022-03-10 23:49:38 +0100

    Fixed the error message..

--- javascript.js
+++ javascript.js
@@ -1,27 +1,35 @@
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmpassword');
 const passwordDiv = document.querySelector('#comparePW');
-const passwordDivChildren = passwordDiv.childNodes;
+// const passwordDivChildren = passwordDiv.childNodes;
+const errorDiv = document.getElementById("error");
+errorDiv.style.color = "red";
+const errorDivChildren = errorDiv.childNodes;
+
 
 function validatePassword(){
     if(password != confirmPassword && password.value.length != 0){
+        
         // make a paragraph and remove all paras.. i guess?
         if(passwordDiv.innerHTML.indexOf("*") !== 145){
-            passwordDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
-            //const textToAdd = document.createTextNode("* The two passwords are not the same!");
-            //textToAdd.style.color = "red";
+            errorDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
+            const textToAdd = document.createTextNode("* The passwords do not match!");
+            // textToAdd.style.color = "red";
             //passwordDiv.appendChild(textToAdd);
-            password.after("* The two passwords are not the same!");
+            errorDiv.appendChild(textToAdd);
+
+            // errorDiv.before("* The passwords don't match!");
+            // password.after("* The passwords don't match!");
         }
 
     }
     if(password.value === confirmPassword.value && password.value.length > 0){
-        passwordDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
+        errorDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
 
     }
 
     if(password.value.length === 0 && confirmPassword.value.length === 0){
-        passwordDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
+        errorDivChildren.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
     }
 
 };
